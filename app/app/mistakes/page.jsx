6d9fb5c72f957ed3b97747f4099568faefeb1af1
felhalen <popazovtimur@gmail.com>
2026-05-15 13:44:03 +0300

    Create MistakesPage component with error analysis content

---
+++ app/app/mistakes/page.jsx
@@ -0,0 +1,8 @@
+export default function MistakesPage() {
+  return (
+    <div style={{ color: "white", padding: 40 }}>
+      <h1>Ошибки</h1>
+      <p>Разбор ошибок в сделках.</p>
+    </div>
+  )
+}
