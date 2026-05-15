ff9673054b1fbec5296ffc9028735fc4e2a88afb
felhalen <popazovtimur@gmail.com>
2026-05-15 13:46:29 +0300

    Add EquityPage component with basic layout

---
+++ app/equity/page.jsx
@@ -0,0 +1,8 @@
+export default function EquityPage() {
+  return (
+    <div style={{ color: "white", padding: 40 }}>
+      <h1>Equity Curve</h1>
+      <p>График роста депозита.</p>
+    </div>
+  )
+}
