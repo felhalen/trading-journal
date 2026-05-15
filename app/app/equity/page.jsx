f6aeac054e42751a46e0187310e984e6f26e8ae8
felhalen <popazovtimur@gmail.com>
2026-05-15 13:46:09 +0300

    Delete app/app directory

--- app/app/equity/page.jsx
+++
@@ -1,8 +0,0 @@
-export default function EquityPage() {
-  return (
-    <div style={{ color: "white", padding: 40 }}>
-      <h1>Equity Curve</h1>
-      <p>График роста депозита.</p>
-    </div>
-  )
-}
