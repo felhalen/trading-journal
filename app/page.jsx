1d469581cd20a3d9cb034535fbef01f1c47a930f
felhalen <popazovtimur@gmail.com>
2026-05-15 13:35:27 +0300

    Fix import formatting in page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,6 @@
-'use client'import Link from "next/link"
+'use client'
+
+import Link from "next/link"
 
 import { useEffect, useMemo, useState } from 'react'
 import { supabase } from '@/lib/supabaseClient'
