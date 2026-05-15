e4d5c0f676e9db3fa8f962f6efb1e68c811ecf2a
felhalen <popazovtimur@gmail.com>
2026-05-15 13:45:45 +0300

    Create TradesPage component for displaying trades

---
+++ app/trades/page.jsx
@@ -0,0 +1,8 @@
+export default function TradesPage() {
+  return (
+    <div style={{ color: "white", padding: 40 }}>
+      <h1>Все сделки</h1>
+      <p>История всех сделок.</p>
+    </div>
+  )
+}
